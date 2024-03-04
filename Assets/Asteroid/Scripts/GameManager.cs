c083407713f2cbd978a86d2e3000c4be87037d63
muhsinabdil <muhsinabdil@hotmail.com>
2024-03-04 15:07:05 +0300

    created game manager

--- Assets/Asteroid/Scripts/GameManager.cs
+++ Assets/Asteroid/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    public static GameManager instance;//! singeltona dönüştürmek için
+    public enum GameState { Menu, Game, LevelComplete, GameOver } //! oyun durumları
+
+    public static Action<GameState> onGameStateChanged;//! oyun durumunu değiştirecek Action
+                                                       // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            //! başka bir oyun yöneticisi var ise
+            Destroy(gameObject);//! bu oyun yöneticisini yok et
+
+        }
+        else
+        {
+            instance = this;
+        }
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
